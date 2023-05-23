1f93bdf5ac0ff303c357de07822781ed777e766c
Respirens <thesergiyprotsanin@gmail.com>
2023-05-23 19:35:42 +0300

    Created NATS client and message handler

--- src/account/__main__.py
+++ src/account/__main__.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import os
 
 from account.service import AccountService
 
@@ -8,13 +7,14 @@ from account.service import AccountService
 async def main():
     logging.basicConfig(level=logging.INFO)
 
-    nats_servers = os.getenv("NATS_SERVERS").split(",")
-    if nats_servers is None or len(nats_servers) == 0:
-        raise RuntimeError("NATS_SERVERS is not defined")
-
-    service = AccountService(nats_servers)
+    service = AccountService("t.account")
     await service.start()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
--- src/account/service.py
+++ src/account/service.py
@@ -1,18 +1,48 @@
+import asyncio
+import logging
+import signal
+
 import nats
 from nats.aio.client import Client
+from nats.aio.msg import Msg
+from nats.aio.subscription import Subscription
 
 
 class AccountService:
-    def __init__(self, servers: list[str]):
-        self.servers: list[str] = servers
-        self.handlers: list[MessageListener]
+    def __init__(self, name: str):
+        self.name: str = name
         self.nats_client: Client | None = None
+        self.subscription: Subscription | None = None
 
-    async def connect(self):
-        self.nats_client = await nats.connect(self.servers)
+    @staticmethod
+    async def _message_handler(msg: Msg):  # For test only
+        logging.info(f"Received msg from {msg.subject}: {msg.data.decode()}")
+        await msg.respond(f"Hello from service! Echo: {msg.data.decode()}".encode())
+
+    async def connect(self, servers: list[str]):
+        logging.info("Connecting to NATS servers")
+        self.nats_client = await nats.connect(servers)
 
     async def start(self):
-        pass  # TODO: start service
+        if self.nats_client is None or self.nats_client.is_closed:
+            await self.connect(["nats://127.0.0.1:4222"])  # Temporary hardcoded nats address :)
+        logging.info("Starting service")
+        self.subscription = await self.nats_client.subscribe(f"{self.name}.*", cb=AccountService._message_handler)
+        logging.info("Configuring signal handlers (SIGINT, SIGTERM)")
+
+        async def signal_handler():
+            await self.stop()
+            asyncio.get_running_loop().stop()
+
+        loop = asyncio.get_running_loop()
+        loop.add_signal_handler(signal.SIGINT, lambda: asyncio.create_task(signal_handler()))
+        loop.add_signal_handler(signal.SIGTERM, lambda: asyncio.create_task(signal_handler()))
 
     async def stop(self):
-        pass  # TODO: stop service
+        logging.info("Initiating shutdown")
+        if self.subscription is None:
+            logging.warning("Service is not running!")
+        else:
+            await self.subscription.unsubscribe()
+        await self.nats_client.drain()
+        logging.info("Service exiting")
